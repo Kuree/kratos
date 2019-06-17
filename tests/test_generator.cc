93648d31b514b2219e8ba317e980923cb7857ebf
Keyi Zhang <keyi@stanford.edu>
2019-06-17 10:51:27 -0700

    restruct the tests

--- tests/test_generator.cc
+++ tests/test_generator.cc
@@ -1,4 +1,3 @@
-#include "../src/expr.hh"
 #include "../src/generator.hh"
 #include "gtest/gtest.h"
 
@@ -22,34 +21,4 @@ TEST(generator, port) {  // NOLINT
     auto mod = c.generator("module");
     mod.port(PortDirection::In, "in", 1);
     mod.port(PortDirection::Out, "out", 1);
-}
-
-TEST(generator, expr) {  // NOLINT
-    Context c;
-    auto mod = c.generator("module");
-    Port &p_in = mod.port(PortDirection::In, "in", 1);
-    Port &p_out = mod.port(PortDirection::Out, "out", 1);
-
-    Var &var1 = mod.var("a", 1);
-    Var var2 = mod.var("b", 1);
-    auto expr = var1 + var2;
-    EXPECT_EQ(expr.left.get(), &var1);
-    // var2 is stored in stack
-    EXPECT_NE(expr.right.get(), &var2);
-
-    expr = p_in + p_out;
-    EXPECT_EQ(expr.name, "(in + out)");
-
-    expr = (var1 - var2).ashr(var2);
-    EXPECT_EQ(expr.name, "((a - b) >>> b)");
-
-    // test auto collapsing
-    auto &expr1 = var1 - var2;
-    auto &expr2 = var1 - var2;
-    // they should point to the same memory address
-    EXPECT_EQ(&expr1, &expr2);
-
-    // test unary
-    auto expr_unary = -var1;
-    EXPECT_EQ(expr_unary.name, "(- a)");
 }
