6d403fc8a2a2a6750d663b3961439354a9b42236
Keyi Zhang <keyi@stanford.edu>
2019-07-06 17:52:05 -0700

    bump release version

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     packages=[
         "kratos"
     ],
-    version='0.0.1',
+    version='0.0.2',
     author='Keyi Zhang',
     author_email='keyi@stanford.edu',
     description='Kratos is a fast hardware design language embedded in Python',
