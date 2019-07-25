351326787d36fecac01969719733f8537f4814b4
Keyi Zhang <keyi@stanford.edu>
2019-07-24 21:49:54 -0700

    split python bindings to multiple files to speed up the compile time

--- python/kratos.cc
+++ python/kratos.cc
@@ -1,7 +1,3 @@
-#include <pybind11/functional.h>
-#include <pybind11/operators.h>
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
 #include "../src/codegen.hh"
 #include "../src/except.hh"
 #include "../src/expr.hh"
@@ -11,6 +7,7 @@
 #include "../src/util.hh"
 
 #include "kratos_expr.hh"
+#include "kratos_stmt.hh"
 
 namespace py = pybind11;
 using std::shared_ptr;
@@ -190,89 +187,6 @@ void init_util(py::module &m) {
              "have to have either verilator or iverilog in your $PATH to use this function");
 }
 
-template <typename T, typename K>
-void def_trace(T &class_) {
-    class_.def("add_fn_ln", [](K &var, const std::pair<std::string, uint32_t> &info) {
-        var.fn_name_ln.emplace_back(info);
-    });
-}
-
-template <typename T>
-void init_var_base(py::class_<T, std::shared_ptr<T>> &class_) {
-    init_common_expr<py::class_<T, std::shared_ptr<T>>, Var>(class_);
-}
-
-template <typename T>
-void init_var_derived(py::class_<T, std::shared_ptr<T>, Var> &class_) {
-    init_common_expr<py::class_<T, std::shared_ptr<T>, Var>, T>(class_);
-    class_.def(
-        "assign",
-        [](const std::shared_ptr<T> &var_to, const std::shared_ptr<Var> &var_from,
-           AssignmentType type) -> auto { return var_to->assign(var_from, type); },
-        py::return_value_policy::reference);
-}
-
-// deal with all the expr/var stuff
-void init_expr(py::module &m) {
-    auto var = py::class_<Var, ::shared_ptr<Var>>(m, "Var");
-    init_var_base(var);
-    def_trace<py::class_<Var, ::shared_ptr<Var>>, Var>(var);
-
-    auto expr = py::class_<Expr, ::shared_ptr<Expr>, Var>(m, "Expr");
-    init_var_derived(expr);
-    def_trace<py::class_<Expr, ::shared_ptr<Expr>, Var>, Expr>(expr);
-
-    auto port = py::class_<Port, ::shared_ptr<Port>, Var>(m, "Port");
-    init_var_derived(port);
-    port.def("port_direction", &Port::port_direction).def("port_type", &Port::port_type);
-    def_trace<py::class_<Port, ::shared_ptr<Port>, Var>, Port>(port);
-
-    auto const_ = py::class_<Const, ::shared_ptr<Const>, Var>(m, "Const");
-    init_var_derived(const_);
-    const_.def("value", &Const::value).def("set_value", &Const::set_value);
-    def_trace<py::class_<Const, ::shared_ptr<Const>, Var>, Const>(const_);
-
-    auto slice = py::class_<VarSlice, ::shared_ptr<VarSlice>, Var>(m, "VarSlice");
-    init_var_derived(slice);
-    def_trace<py::class_<VarSlice, ::shared_ptr<VarSlice>, Var>, VarSlice>(slice);
-
-    auto concat = py::class_<VarConcat, ::shared_ptr<VarConcat>, Var>(m, "VarConcat");
-    init_var_derived(concat);
-    def_trace<py::class_<VarConcat, ::shared_ptr<VarConcat>, Var>, VarConcat>(concat);
-
-    auto param = py::class_<Param, ::shared_ptr<Param>, Var>(m, "Param");
-    init_var_derived(param);
-    param.def("value", &Param::value).def("set_value", &Param::set_value);
-    def_trace<py::class_<Param, ::shared_ptr<Param>, Var>, Param>(param);
-
-    auto port_packed = py::class_<PortPacked, ::shared_ptr<PortPacked>, Var>(m, "PortPacked");
-    init_var_derived(port_packed);
-    port_packed.def("port_direction", &PortPacked::port_direction)
-        .def("port_type", &PortPacked::port_type)
-        .def(
-            "__getitem__",
-            [](PortPacked & port, const std::string &name) -> auto & { return port[name]; },
-            py::return_value_policy::reference);
-    def_trace<py::class_<PortPacked, ::shared_ptr<PortPacked>, Var>, PortPacked>(port_packed);
-
-    // struct info for packed
-    auto struct_ = py::class_<PackedStruct>(m, "PackedStruct");
-    struct_.def(py::init<std::string, std::vector<std::tuple<std::string, uint32_t, bool>>>())
-        .def_readonly("struct_name", &PackedStruct::struct_name)
-        .def_readonly("attributes", &PackedStruct::attributes);
-
-    auto port_slice =
-        py::class_<PortPackedSlice, ::shared_ptr<PortPackedSlice>, Var>(m, "PortPackedSlice");
-    init_var_derived(port_slice);
-    def_trace<py::class_<PortPackedSlice, ::shared_ptr<PortPackedSlice>, Var>, VarSlice>(
-        port_slice);
-
-    auto array = py::class_<Array, ::shared_ptr<Array>, Var>(m, "Array");
-    init_var_derived(array);
-    def_trace<py::class_<Array, ::shared_ptr<Array>, Var>, Var>(array);
-    array.def("size", &Array::size);
-}
-
 void init_context(py::module &m) {
     auto context = py::class_<Context>(m, "Context");
     context.def(py::init())
@@ -357,72 +271,7 @@ void init_generator(py::module &m) {
     });
 }
 
-void init_stmt(py::module &m) {
-    py::class_<Stmt, ::shared_ptr<Stmt>> stmt_(m, "Stmt");
-    stmt_.def(py::init<StatementType>()).def("type", &Stmt::type);
-    def_trace<py::class_<Stmt, ::shared_ptr<Stmt>>, Stmt>(stmt_);
 
-    py::class_<AssignStmt, ::shared_ptr<AssignStmt>, Stmt> assign_(
-        m, "AssignStmt", R"pbdoc(Assignment statement)pbdoc");
-    assign_.def("assign_type", &AssignStmt::assign_type)
-        .def("set_assign_type", &AssignStmt::set_assign_type)
-        .def_property_readonly("left", [](const AssignStmt &stmt) { return stmt.left(); })
-        .def_property_readonly("right", [](const AssignStmt &stmt) { return stmt.right(); });
-
-    def_attributes<py::class_<AssignStmt, ::shared_ptr<AssignStmt>, Stmt>, AssignStmt>(assign_);
-
-    py::class_<IfStmt, ::shared_ptr<IfStmt>, Stmt>(m, "IfStmt")
-        .def(py::init<::shared_ptr<Var>>())
-        .def("predicate", &IfStmt::predicate, py::return_value_policy::reference)
-        .def("then_body", &IfStmt::then_body)
-        .def("else_body", &IfStmt::else_body)
-        .def("add_then_stmt", py::overload_cast<const ::shared_ptr<Stmt> &>(&IfStmt::add_then_stmt))
-        .def("add_else_stmt", py::overload_cast<const ::shared_ptr<Stmt> &>(&IfStmt::add_else_stmt))
-        .def("remove_then_stmt",
-             py::overload_cast<const ::shared_ptr<Stmt> &>(&IfStmt::remove_then_stmt))
-        .def("remove_else_stmt",
-             py::overload_cast<const ::shared_ptr<Stmt> &>(&IfStmt::remove_else_stmt));
-
-    py::class_<SwitchStmt, ::shared_ptr<SwitchStmt>, Stmt>(m, "SwitchStmt")
-        .def(py::init<const ::shared_ptr<Var> &>())
-        .def("add_switch_case",
-             py::overload_cast<const std::shared_ptr<Const> &, const std::shared_ptr<Stmt> &>(
-                 &SwitchStmt::add_switch_case))
-        .def("add_switch_case", py::overload_cast<const std::shared_ptr<Const> &,
-                                                  const std::vector<std::shared_ptr<Stmt>> &>(
-                                    &SwitchStmt::add_switch_case))
-        .def("target", &SwitchStmt::target, py::return_value_policy::reference)
-        .def("body", &SwitchStmt::body)
-        .def("remove_switch_case", py::overload_cast<const std::shared_ptr<kratos::Const> &>(
-                                       &SwitchStmt::remove_switch_case))
-        .def("remove_switch_case",
-             py::overload_cast<const std::shared_ptr<Const> &, const std::shared_ptr<Stmt> &>(
-                 &SwitchStmt::remove_switch_case));
-
-    py::class_<CombinationalStmtBlock, ::shared_ptr<CombinationalStmtBlock>, Stmt>(
-        m, "CombinationalStmtBlock")
-        .def(py::init<>())
-        .def("block_type", &CombinationalStmtBlock::block_type)
-        .def("add_stmt",
-             py::overload_cast<const ::shared_ptr<Stmt> &>(&CombinationalStmtBlock::add_stmt))
-        .def("remove_stmt", &CombinationalStmtBlock::remove_stmt);
-
-    py::class_<ScopedStmtBlock, ::shared_ptr<ScopedStmtBlock>, Stmt>(m, "ScopedStmtBlock")
-        .def(py::init<>());
-
-    py::class_<SequentialStmtBlock, ::shared_ptr<SequentialStmtBlock>, Stmt>(m,
-                                                                             "SequentialStmtBlock")
-        .def(py::init<>())
-        .def("get_conditions", &SequentialStmtBlock::get_conditions)
-        .def("add_condition", &SequentialStmtBlock::add_condition)
-        .def("add_stmt",
-             py::overload_cast<const ::shared_ptr<Stmt> &>(&SequentialStmtBlock::add_stmt))
-        .def("remove_stmt", &SequentialStmtBlock::remove_stmt);
-
-    py::class_<ModuleInstantiationStmt, ::shared_ptr<ModuleInstantiationStmt>, Stmt>(
-        m, "ModuleInstantiationStmt")
-        .def(py::init<Generator *, Generator *>());
-}
 
 void init_code_gen(py::module &m) {
     py::class_<VerilogModule>(m, "VerilogModule")
---
+++ python/kratos_stmt.cc
@@ -0,0 +1,83 @@
+#include <pybind11/functional.h>
+#include <pybind11/operators.h>
+#include <pybind11/pybind11.h>
+#include "../src/except.hh"
+#include "../src/expr.hh"
+#include "../src/generator.hh"
+#include "../src/stmt.hh"
+
+#include "kratos_expr.hh"
+
+namespace py = pybind11;
+using std::shared_ptr;
+using namespace kratos;
+
+namespace kratos {
+
+void init_stmt(py::module &m) {
+    py::class_<Stmt, ::shared_ptr<Stmt>> stmt_(m, "Stmt");
+    stmt_.def(py::init<StatementType>()).def("type", &Stmt::type);
+    def_trace<py::class_<Stmt, ::shared_ptr<Stmt>>, Stmt>(stmt_);
+
+    py::class_<AssignStmt, ::shared_ptr<AssignStmt>, Stmt> assign_(
+        m, "AssignStmt", R"pbdoc(Assignment statement)pbdoc");
+    assign_.def("assign_type", &AssignStmt::assign_type)
+        .def("set_assign_type", &AssignStmt::set_assign_type)
+        .def_property_readonly("left", [](const AssignStmt &stmt) { return stmt.left(); })
+        .def_property_readonly("right", [](const AssignStmt &stmt) { return stmt.right(); });
+
+    def_attributes<py::class_<AssignStmt, ::shared_ptr<AssignStmt>, Stmt>, AssignStmt>(assign_);
+
+    py::class_<IfStmt, ::shared_ptr<IfStmt>, Stmt>(m, "IfStmt")
+        .def(py::init<::shared_ptr<Var>>())
+        .def("predicate", &IfStmt::predicate, py::return_value_policy::reference)
+        .def("then_body", &IfStmt::then_body)
+        .def("else_body", &IfStmt::else_body)
+        .def("add_then_stmt", py::overload_cast<const ::shared_ptr<Stmt> &>(&IfStmt::add_then_stmt))
+        .def("add_else_stmt", py::overload_cast<const ::shared_ptr<Stmt> &>(&IfStmt::add_else_stmt))
+        .def("remove_then_stmt",
+             py::overload_cast<const ::shared_ptr<Stmt> &>(&IfStmt::remove_then_stmt))
+        .def("remove_else_stmt",
+             py::overload_cast<const ::shared_ptr<Stmt> &>(&IfStmt::remove_else_stmt));
+
+    py::class_<SwitchStmt, ::shared_ptr<SwitchStmt>, Stmt>(m, "SwitchStmt")
+        .def(py::init<const ::shared_ptr<Var> &>())
+        .def("add_switch_case",
+             py::overload_cast<const std::shared_ptr<Const> &, const std::shared_ptr<Stmt> &>(
+                 &SwitchStmt::add_switch_case))
+        .def("add_switch_case", py::overload_cast<const std::shared_ptr<Const> &,
+                                                  const std::vector<std::shared_ptr<Stmt>> &>(
+                                    &SwitchStmt::add_switch_case))
+        .def("target", &SwitchStmt::target, py::return_value_policy::reference)
+        .def("body", &SwitchStmt::body)
+        .def("remove_switch_case", py::overload_cast<const std::shared_ptr<kratos::Const> &>(
+                                       &SwitchStmt::remove_switch_case))
+        .def("remove_switch_case",
+             py::overload_cast<const std::shared_ptr<Const> &, const std::shared_ptr<Stmt> &>(
+                 &SwitchStmt::remove_switch_case));
+
+    py::class_<CombinationalStmtBlock, ::shared_ptr<CombinationalStmtBlock>, Stmt>(
+        m, "CombinationalStmtBlock")
+        .def(py::init<>())
+        .def("block_type", &CombinationalStmtBlock::block_type)
+        .def("add_stmt",
+             py::overload_cast<const ::shared_ptr<Stmt> &>(&CombinationalStmtBlock::add_stmt))
+        .def("remove_stmt", &CombinationalStmtBlock::remove_stmt);
+
+    py::class_<ScopedStmtBlock, ::shared_ptr<ScopedStmtBlock>, Stmt>(m, "ScopedStmtBlock")
+        .def(py::init<>());
+
+    py::class_<SequentialStmtBlock, ::shared_ptr<SequentialStmtBlock>, Stmt>(m,
+                                                                             "SequentialStmtBlock")
+        .def(py::init<>())
+        .def("get_conditions", &SequentialStmtBlock::get_conditions)
+        .def("add_condition", &SequentialStmtBlock::add_condition)
+        .def("add_stmt",
+             py::overload_cast<const ::shared_ptr<Stmt> &>(&SequentialStmtBlock::add_stmt))
+        .def("remove_stmt", &SequentialStmtBlock::remove_stmt);
+
+    py::class_<ModuleInstantiationStmt, ::shared_ptr<ModuleInstantiationStmt>, Stmt>(
+        m, "ModuleInstantiationStmt")
+        .def(py::init<Generator *, Generator *>());
+}
+}
