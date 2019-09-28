4060a94a090c5a63ac3094ed8225ce5492e4f1a4
Keyi Zhang <keyi@stanford.edu>
2019-09-27 18:37:50 -0700

    allow debug info being inserted at the beginning

--- python/kratos_debug.hh
+++ python/kratos_debug.hh
@@ -10,6 +10,13 @@ void def_trace(T &class_) {
     class_.def("add_fn_ln", [](K &var, const std::pair<std::string, uint32_t> &info) {
       var.fn_name_ln.emplace_back(info);
     })
+    .def("add_fn_ln", [](K &var, const std::pair<std::string, uint32_t> &info, bool insert_front) {
+        if (insert_front) {
+            var.fn_name_ln.insert(var.fn_name_ln.begin(), info);
+        } else {
+            var.fn_name_ln.emplace_back(info);
+        }
+    })
     .def_readwrite("comment", &K::comment)
     .def_readwrite("fn_name_ln", &K::fn_name_ln);
 }
