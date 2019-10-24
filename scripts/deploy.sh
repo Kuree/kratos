aea82b00b95ad553e8c35dc0930e452bf3803b31
Keyi Zhang <keyi@stanford.edu>
2019-10-24 10:29:47 -0700

    add python3.8 deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -e
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     if [[ "$BUILD_WHEEL" == true ]]; then
         docker cp ~/.pypirc manylinux:/home/
-        docker exec -i manylinux bash -c 'cd /kratos && for PYBIN in cp35 cp36 cp37; do /opt/python/${PYBIN}-${PYBIN}m/bin/python setup.py bdist_wheel; done'
+        docker exec -i manylinux bash -c 'cd /kratos && for PYBIN in cp35 cp36 cp37 cp38; do /opt/python/${PYBIN}-${PYBIN}m/bin/python setup.py bdist_wheel; done'
         docker exec -i manylinux bash -c 'cd /kratos && for WHEEL in dist/*.whl; do auditwheel repair "${WHEEL}"; done'
         docker exec -i manylinux bash -c 'cd /kratos && twine upload --config-file /home/.pypirc wheelhouse/*'
     fi
