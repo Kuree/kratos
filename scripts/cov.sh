8d63586b4e8ef1c85d9273d7b2c010984af90a1a
Keyi Zhang <keyi@stanford.edu>
2020-01-22 21:53:13 -0800

    simplify the cov [skip ci]

--- scripts/cov.sh
+++ scripts/cov.sh
@@ -10,12 +10,8 @@ docker exec -i cov bash -c "cd /kratos/build/temp.linux-x86_64-3.7 && make test"
 docker exec -i cov bash -c "python3 -m pip install pytest pytest-cov && cd /kratos && pytest --cov-report=xml --cov=kratos tests/"
 docker exec -i cov bash -c "cd kratos && lcov --capture --directory . --output-file coverage.info"
 # clean up coverage
-docker exec -i cov bash -c "cd kratos && lcov --remove coverage.info '/usr/*' --output-file coverage.info"
-docker exec -i cov bash -c "cd kratos && lcov --remove coverage.info '*extern*' --output-file coverage.info"
+docker exec -i cov bash -c "cd kratos && lcov --remove coverage.info '/usr/*' '*extern*' '*tests*' --output-file coverage.info"
 docker exec -i cov bash -c "cd kratos && lcov --list coverage.info"
-docker cp cov:/kratos/coverage.info coverage.info
-docker cp cov:/kratos/.coverage .coverage
 
 # upload the coverage files
-bash <(curl -s https://codecov.io/bash) -f coverage.info
 bash <(curl -s https://codecov.io/bash)
