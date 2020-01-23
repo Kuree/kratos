ed513564c108aa9d23905e74f0d4f6e058a4f53b
Keyi Zhang <keyi@stanford.edu>
2020-01-22 17:33:38 -0800

    add python coverage as well [skip ci]

--- scripts/cov.sh
+++ scripts/cov.sh
@@ -2,16 +2,21 @@
 set -e
 
 docker run -it -d --name cov --mount type=bind,source="$(pwd)"/../kratos,target=/kratos keyiz/garnet-flow bash
+docker exec -i cov bash -c "apt update && apt install -y lcov"
 docker exec -i cov bash -c "cd /kratos && KRATOS_COVERAGE=1 python3 -m pip install -e ."
 docker exec -i cov bash -c "cd /kratos/build/temp.linux-x86_64-3.7 && make -j2"
 docker exec -i cov bash -c "cd /kratos/build/temp.linux-x86_64-3.7 && make test"
-docker exec -i cov bash -c "python3 -m pip install pytest && cd /kratos && pytest tests/"
+# do not collect tests
+docker exec -i cov bash -c "echo -e '[run]\nomit = tests/*' > .coveragerc && cat /.coveragerc"
+docker exec -i cov bash -c "python3 -m pip install pytest pytest-cov && cd /kratos && pytest --cov=kratos --cov-config=/.coveragerc tests/"
 docker exec -i cov bash -c "cd kratos && lcov --capture --directory . --output-file coverage.info"
 # clean up coverage
 docker exec -i cov bash -c "cd kratos && lcov --remove coverage.info '/usr/*' --output-file coverage.info"
 docker exec -i cov bash -c "cd kratos && lcov --remove coverage.info '*extern*' --output-file coverage.info"
 docker exec -i cov bash -c "cd kratos && lcov --list coverage.info"
 docker cp cov:/kratos/coverage.info coverage.info
+docker cp cov:/kratos/.coverage .coverage
 
-# upload the coverage file
+# upload the coverage files
 bash <(curl -s https://codecov.io/bash) -f coverage.info
+bash <(curl -s https://codecov.io/bash) -f .coverage
