8c62bffea740501dae4575fc83d99ec9ebc504b9
Keyi Zhang <keyi@stanford.edu>
2020-01-30 00:42:10 -0800

    fix cov

--- scripts/cov.sh
+++ scripts/cov.sh
@@ -9,7 +9,7 @@ docker exec -i cov bash -c "cd /kratos/build/temp.linux-x86_64-3.7 && make test"
 docker exec -i cov bash -c "python3 -m pip install pytest pytest-cov && cd /kratos && pytest --cov-report=xml --cov=kratos tests/"
 docker exec -i cov bash -c "cd kratos && lcov --capture --directory . --output-file coverage.info"
 # clean up coverage
-docker exec -i cov bash -c "cd kratos && lcov --remove coverage.info '/usr/*' '*extern*' '*tests*' '*python*'--output-file coverage.info"
+docker exec -i cov bash -c "cd kratos && lcov --remove coverage.info '/usr/*' '*extern*' '*tests*' '*python*' --output-file coverage.info"
 docker exec -i cov bash -c "cd kratos && lcov --list coverage.info"
 
 # upload the coverage files
