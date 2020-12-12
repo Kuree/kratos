358d6085871a74093f41488b46efe1aeb462cf12
Keyi Zhang <keyi@stanford.edu>
2020-12-12 15:07:07 -0800

    update base image

--- scripts/cov.sh
+++ scripts/cov.sh
@@ -3,8 +3,8 @@ set -e
 
 docker run -it -d --name cov --mount type=bind,source="$(pwd)"/../kratos,target=/kratos keyiz/kratos:test bash
 docker exec -i cov bash -c "cd /kratos && KRATOS_COVERAGE=1 python3 -m pip install -e ."
-docker exec -i cov bash -c "cd /kratos/build/temp.linux-x86_64-3.7 && make -j2"
-docker exec -i cov bash -c "cd /kratos/build/temp.linux-x86_64-3.7 && make test"
+docker exec -i cov bash -c "cd /kratos/build/temp.linux-x86_64-3.8 && make -j2"
+docker exec -i cov bash -c "cd /kratos/build/temp.linux-x86_64-3.8 && make test"
 # do not collect tests
 docker exec -i cov bash -c "python3 -m pip install pytest pytest-cov && cd /kratos && pytest --cov-report=xml --cov=kratos tests/"
 docker exec -i cov bash -c "cd kratos && lcov --capture --directory . --output-file coverage.info"
